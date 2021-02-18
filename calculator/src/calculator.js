7edab3d18d67b832f1c7b7cc96c6c4901a3548fa
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-18 16:22:15 +0530

    removing duplication using 'apply'

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,11 +1,6 @@
 function add(){
     function parseArg(n){
-        if (Array.isArray(n)){
-            var result = 0;
-            for(var index = 0, count = n.length; index < count; index++)
-                result += parseArg(n[index]);
-            return result;
-        }
+        if (Array.isArray(n)) return add.apply(this, n);
         if (typeof n === 'function') return parseArg(n());
         return isNaN(n) ? 0 : parseInt(n,10);
     }
