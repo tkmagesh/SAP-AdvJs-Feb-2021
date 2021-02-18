d2b911860ffb206ea53d0799ea397ce86b9d80e5
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-18 18:37:44 +0530

    spinner solution

---
+++ closures/spinner.js
@@ -0,0 +1,36 @@
+var spinner = (function(){
+    var counter = 0;
+
+    function up(){
+        return ++counter;
+    }
+
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    }
+})();
+
+
+
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+spinner.up() //=> 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+
+counter = 10000;
+spinner.up() // => SHOULE NOT return 10001
+
+spinner.counter = 10000;
+spinner.up() // => SHOULE NOT return 10001
