2a2e5fa577f94ce7c3d55a018bf3513a4b4ec39d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-19 20:25:21 +0530

    Promise.all & Promise.allSettled example

--- async/async-demos.js
+++ async/async-demos.js
@@ -46,6 +46,7 @@
         return p;
     }
 
+    globalThis['addAsyncPromise'] = addAsyncPromise;
     /* 
     function addAsyncPromiseClient(x,y){
         console.log(`[@client] triggering the service`);
@@ -60,8 +61,34 @@
         console.log(`[@client] triggering the service`);
         var result = await addAsyncPromise(x,y);
         console.log(`[@client] result = ${result}`);
+        var result2 = await addAsyncPromise(300,500);
+        console.log(`[@client] result2 = ${result2}`);
     }
 
     globalThis['addAsyncPromiseClient'] = addAsyncPromiseClient;
 
+    function divideAsyncPromise(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`);
+        var p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){ 
+                if (y === 0){
+                    return rejectFn(new Error('Invalid arguments!'));
+                }
+                var result = x / y;
+                console.log(`   [@service] return result`);
+                resolveFn(result);
+            }, 7000);
+        });
+        return p;
+    }
+
+    globalThis['divideAsyncPromise'] = divideAsyncPromise;
+
 })();
+
+//using allSettled
+var x = 100, y = 0;
+var p1 = addAsyncPromise(x,y);
+var p2 = divideAsyncPromise(x,y);
+Promise.allSettled([p1, p2])
+    .then(([addResult, divideResult]) => console.log(addResult, divideResult));
